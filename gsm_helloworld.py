99302aa19de0f5379b5d02997c0cd82a05687b89
guv114 <68322350+guv114@users.noreply.github.com>
2020-08-23 18:48:41 +0100

    second changes to helloworld
    This is my second change that i have made to my first line of code ever!! Very exciting to start coding and use github to showcase my progression

--- gsm_helloworld.py
+++ gsm_helloworld.py
@@ -1,1 +1,6 @@
+# My first Commit and code..very excited!! 2020
 print("HelloWorld")
+print("This is my first change, which is being uploaded to git hub")
+print()
+# I am testing my file to see if changes are being uplaoed to github
+print("and this is my second official change on github, from local remote internet")
